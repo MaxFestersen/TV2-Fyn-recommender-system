c8f2925f86fc878d6f30af0763545023e0c4debe
ADernild <alex@dernild.dk>
2022-02-08 16:32:49 +0100

    making sure scrollY can't be overwritten by smaller value

--- cookies/public/php/session_update.php
+++ cookies/public/php/session_update.php
@@ -11,7 +11,7 @@ $scrollY=$_POST['scrollY'];
 $sql = "INSERT INTO `session`(`sessionID`, `elapsed`, `articleID`, `scrollY`)
 VALUES ('{$sessionID}', '{$elapsed}', '{$articleID}', '{$scrollY}')
 ON DUPLICATE KEY UPDATE
-`elapsed`= VALUES(`elapsed`) + `elapsed`, `scrollY`='{$scrollY}';";
+`elapsed`= VALUES(`elapsed`) + `elapsed`, `scrollY`= GREATEST(VALUES(`scrollY`), `scrollY`);";
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
   } else {
