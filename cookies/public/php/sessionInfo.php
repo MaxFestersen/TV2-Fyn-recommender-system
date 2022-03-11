9c0b0ce06ea76cb6c67c148a941c2dd1f0375195
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2022-03-11 15:56:59 +0100

    Fixed problem with attempting to update when entry exists, by checking if it does, and only allowing while it doesn't.

--- cookies/public/php/sessionInfo.php
+++ cookies/public/php/sessionInfo.php
@@ -3,10 +3,6 @@ header("Access-Control-Allow-Origin: *"); // change * to http://tv2fyn.dk
 header('Content-type: text/plain; charset=utf-8'); // Set charset to utf-8
 include(dirname(__DIR__).'../../config/database.php');
 
-// PREPARE
-$stmt = $conn -> prepare("INSERT INTO `sessionInfo`(`sessionID`, `date`, `elapsed`, `articleID`, `scrollY`, `lat`, `lon`) VALUES (?, ?, ?, ?, ?, ?, ?)");
-$stmt -> bind_param("ssssidd", $sessionID, $date, $elapsed, $articleID, $scrollY, $lat, $lon);
-
 // SET VALUES
 $sessionID=$_POST['sessionID'];
 $date=$_POST['date'];
@@ -16,16 +12,39 @@ $scrollY=$_POST['scrollY'];
 $lat=str_replace(',', '.', $_POST['lat']);
 $lon=str_replace(',', '.', $_POST['lon']);
 
-// EXECUTE & PRINT RESULT
-if ($stmt->execute() === TRUE) {
-	echo "New record created successfully";
-} else {
-	//echo "Error: " . $sql . "<br>" . $conn->error;
-	echo "Error.<br>" . $stmt->error;
+// CHECK IF EXISTS
+// CHECK IF EXISTS: Connection and bind params
+$existsCheck = $conn -> prepare("SELECT COUNT(sessionID) FROM `sessionInfo` WHERE sessionID=? AND articleID=?");
+//$existsCheck = $conn -> prepare("SELECT sessionID, articleID FROM `sessionInfo` WHERE sessionID=? AND articleID=? LIMIT 1");
+$existsCheck -> bind_param("ss", $sessionID, $articleID);
+
+// CHECK IF EXISTS: SET VALUES
+$existsCheck->execute();
+
+// GET RESULTS
+$result = $existsCheck->get_result();
+
+// LOOP RESULTS (allthough it would seem unecesarry, a simple check seems to fail randomly, but that is surely another fault)
+while ($row = $result->fetch_assoc()){
+	//echo $row["COUNT(sessionID)"];
+    if($row["COUNT(sessionID)"]==0){
+		$existsCheck->close();
+		// PREPARE
+		$stmt = $conn -> prepare("INSERT INTO `sessionInfo`(`sessionID`, `date`, `elapsed`, `articleID`, `scrollY`, `lat`, `lon`) VALUES (?, ?, ?, ?, ?, ?, ?)");
+		$stmt -> bind_param("ssssidd", $sessionID, $date, $elapsed, $articleID, $scrollY, $lat, $lon);
+		
+		// EXECUTE & PRINT RESULT
+		if ($stmt->execute() === TRUE) {
+			echo "New record created successfully";
+		} else {
+			//echo "Error: " . $sql . "<br>" . $conn->error;
+			echo "Error.<br>" . $stmt->error;
+		}
+		$stmt->close();
+	}
 }
 
 //CLOSE CONNECTIONS
-$stmt->close();
 $conn->close();
 ?>
 
