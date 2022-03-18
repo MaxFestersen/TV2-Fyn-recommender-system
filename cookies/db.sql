12ab349ba6837810399274eb2e5976425332078b
ADernild <alex@dernild.dk>
2022-03-18 12:30:21 +0100

    database schema changed to contain datetime in sessionInfo

--- cookies/db.sql
+++ cookies/db.sql
@@ -1,18 +1,18 @@
 CREATE DATABASE IF NOT EXISTS tv2fyn;
 
 CREATE TABLE IF NOT EXISTS tv2fyn.device(
-deviceID varchar(255) NOT NULL,
+deviceID VARCHAR(255) NOT NULL,
 firstVisit DATE,
-screenWidth int(10),
-screenHeight int(10),
-deviceOS varchar(255) NOT NULL,
-deviceVendor varchar(255) NOT NULL,
+screenWidth INT(10),
+screenHeight INT(10),
+deviceOS VARCHAR(255) NOT NULL,
+deviceVendor VARCHAR(255) NOT NULL,
 PRIMARY KEY (deviceID)
 );
 
 CREATE TABLE IF NOT EXISTS tv2fyn.session(
-deviceID varchar(255) NOT NULL,
-sessionID varchar(255) NOT NULL,
+deviceID VARCHAR(255) NOT NULL,
+sessionID VARCHAR(255) NOT NULL,
 PRIMARY KEY (sessionID),
 FOREIGN KEY (deviceID) REFERENCES device(deviceID)
 ON DELETE CASCADE
@@ -20,10 +20,10 @@ ON DELETE CASCADE
 
 CREATE TABLE IF NOT EXISTS tv2fyn.sessionInfo(
 sessionID varchar(255) NOT NULL,
-date DATE,
-elapsed time,
-articleID varchar(255) NOT NULL,
-scrollY	int(3),
+date DATETIME,
+elapsed TIME,
+articleID VARCHAR(255) NOT NULL,
+scrollY	INT(3),
 lat FLOAT(6,3),
 lon FLOAT(6,3),
 PRIMARY KEY (sessionID, articleID),
