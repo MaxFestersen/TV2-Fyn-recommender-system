e0fd7f951ef1fcda32e27794dfcc655bd05baa94
ADernild <alex@dernild.dk>
2022-04-11 17:31:15 +0200

    loading database info from .env secret

--- cookies/config/database.php
+++ cookies/config/database.php
@@ -1,15 +1,19 @@
 <?php
 // Connect to database
-$servername = 'db';
-$username = 'recommender';
-$password = rtrim(file_get_contents('/run/secrets/db_pass'));
-$db = 'tv2fyn';
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable('/run/secrets/');
+$dotenv->safeLoad();
+
+$servername = $_ENV['MYSQL_HOST'];
+$username = $_ENV['MYSQL_USER'];
+$password = $_ENV['MYSQL_PASS'];
+$db = $_ENV['MYSQL_DB'];
 
 $conn = new mysqli($servername, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+   die("Connection failed: " . $conn->connect_error);
 }
 ?>
 
---
+++ cookies/index.php
@@ -0,0 +1,3 @@
+<?php 
+echo 'PHP server for cookie implementation in TV2Fyn Recommender System project'
+?>
